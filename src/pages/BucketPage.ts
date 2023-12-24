6696edd13cb7b5f1f55ec84b4604c2b19e288d7d
Aleksandr Gubich <a.gubich@bk.ru>
2023-12-24 17:45:10 +0300

    Logic upd

--- src/pages/BucketPage.ts
+++ src/pages/BucketPage.ts
@@ -6,6 +6,69 @@ templEl.innerHTML = template;
 
 export class BucketPage extends AbstractPage {
   render(): HTMLElement | DocumentFragment {
+    const renderItems = () => {
+      const goodsContainer = document.querySelector('.goods');
+      if (goodsContainer) {
+        goodsContainer.innerHTML = ''; // Очищаем контейнер товаров
+
+        // Получаем товары из local storage
+        const itemsInStorage = localStorage.getItem('items');
+        const items = itemsInStorage ? JSON.parse(itemsInStorage) : [];
+
+        let totalPrice = 0;
+        let totalQuantity = 0;
+
+        // Для каждого товара из local storage создаем элемент в корзине
+        items.forEach((item: any) => {
+          const bucketItem = document.createElement('div');
+          bucketItem.classList.add('bucket-item');
+          bucketItem.innerHTML = `
+            <p>${item.name}</p>
+            <p>${item.price}</p>
+            <p>Quantity: ${item.quantity}</p>
+            <button class="remove-btn">Remove</button>
+          `;
+          goodsContainer.appendChild(bucketItem);
+
+          totalPrice += parseFloat(item.price.replace('$', '')) * item.quantity;
+          totalQuantity += item.quantity;
+        });
+
+        // Обновляем общее количество товаров и общую сумму
+        const productsAmount = document.querySelector('.products_amount');
+        const productsSum = document.querySelector('.products_sum');
+        const priceSum = document.querySelector('.price_sum');
+
+        if (productsAmount && productsSum && priceSum) {
+          productsAmount.textContent = totalQuantity.toString();
+          productsSum.textContent = totalQuantity.toString();
+          priceSum.textContent = `$${totalPrice.toFixed(2)}`;
+        }
+      }
+    };
+
+    renderItems(); // Вызываем функцию отображения товаров при загрузке страницы
+
+    // Устанавливаем слушатель для удаления товаров из корзины
+    const goodsContainer = document.querySelector('.goods');
+    if (goodsContainer) {
+      goodsContainer.addEventListener('click', (event) => {
+        if ((event.target as HTMLElement).classList.contains('remove-btn')) {
+          const itemName = (event.target as HTMLElement).previousElementSibling?.textContent || '';
+          let items = localStorage.getItem('items');
+          if (items) {
+            const parsedItems: { name: string; price: string; quantity?: number }[] = JSON.parse(items);
+            // Убедимся, что items не null перед использованием метода filter
+            if (Array.isArray(parsedItems)) {
+              const updatedItems = parsedItems.filter((item) => item.name !== itemName);
+              localStorage.setItem('items', JSON.stringify(updatedItems));
+            }
+          }
+          renderItems();
+
+        }
+      });
+    }
 
     return templEl.content.cloneNode(true) as DocumentFragment;
   }
--- src/pages/MainPage.ts
+++ src/pages/MainPage.ts
@@ -4,9 +4,79 @@ import template from './MainPage.html'
 const templEl = document.createElement('template');
 templEl.innerHTML = template;
 
-export class MainPage extends AbstractPage {
-  render(): HTMLElement | DocumentFragment {
+// В MainPage.ts
 
+// Определение класса MainPage, который расширяет AbstractPage
+export class MainPage extends AbstractPage {
+  // Метод render возвращает HTMLElement или DocumentFragment
+  render(): HTMLElement | DocumentFragment {
+    // Получаем все кнопки с классом 'toBucket'
+    const toBucketButtons = document.querySelectorAll('.toBucket button');
+
+    // Для каждой кнопки устанавливаем слушатель события 'click'
+    toBucketButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        // Получаем информацию о товаре
+        const product = button.closest('.range');
+        if (product) {
+          const productName = product.querySelector('.name')?.textContent || '';
+          const productPrice = product.querySelector('.price')?.textContent || '';
+
+          // Создаем объект товара
+          const item = { name: productName, price: productPrice };
+
+          // Получаем текущие данные из local storage или создаем новый массив
+          const itemsInStorage = localStorage.getItem('items');
+          const items = itemsInStorage ? JSON.parse(itemsInStorage) : [];
+
+          // Проверяем, есть ли такой товар уже в корзине
+          const existingItem = items.find((item: any) => item.name === productName);
+
+          if (existingItem) {
+            // Если товар уже в корзине, увеличиваем его количество
+            if (existingItem.quantity !== undefined) {
+              existingItem.quantity += 1;
+            } else {
+              existingItem.quantity = 1;
+            }
+          } else {
+            // Иначе добавляем новый товар в массив
+            // Проверяем наличие 'quantity' в объекте item, если нет, устанавливаем его в 1
+            (item as { name: string; price: string; quantity?: number }).quantity = 1;
+            items.push(item);
+
+          }
+
+          // Обновляем local storage
+          localStorage.setItem('items', JSON.stringify(items));
+
+          // Обновляем количество товаров в шапке
+          const productsAmount = document.querySelector('.products_amount');
+          if (productsAmount) {
+            productsAmount.textContent = items.length.toString();
+          }
+        }
+      });
+    });
+
+    // Находим контейнер товаров в корзине
+    const goodsContainer = document.querySelector('.goods');
+    if (goodsContainer) {
+      // Устанавливаем слушатель события 'click' для контейнера товаров в корзине
+      goodsContainer.addEventListener('click', (event) => {
+        // Проверяем, была ли нажата кнопка 'Remove' внутри элемента корзины
+        if ((event.target as HTMLElement).classList.contains('remove-btn')) {
+          // Находим элемент корзины, который нужно удалить
+          const itemToRemove = (event.target as HTMLElement).closest('.bucket-item');
+          if (itemToRemove) {
+            // Удаляем элемент корзины из DOM
+            itemToRemove.remove();
+          }
+        }
+      });
+    }
+
+    // Клонируем содержимое шаблона и возвращаем его в виде DocumentFragment
     return templEl.content.cloneNode(true) as DocumentFragment;
   }
 }
