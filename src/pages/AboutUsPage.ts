bb131a3d79a9f7b489ab0113141ebd74f4aa2f7e
Aleksandr Gubich <a.gubich@bk.ru>
2023-12-25 20:52:41 +0300

    Обновление логики страницы AboutUs

--- src/pages/AboutUsPage.ts
+++ src/pages/AboutUsPage.ts
@@ -1,12 +1,28 @@
 import { AbstractPage } from "../router";
-import template from './AboutUsPage.html'
+import template from './AboutUsPage.html';
 
 const templEl = document.createElement('template');
 templEl.innerHTML = template;
 
 export class AboutUsPage extends AbstractPage {
   render(): HTMLElement | DocumentFragment {
+    const content: DocumentFragment = templEl.content.cloneNode(true) as DocumentFragment;
 
-    return templEl.content.cloneNode(true) as DocumentFragment;
+    const handleUpdateProductsAmount = () => {
+      const productsAmount: Element | null = document.querySelector('.products_sum');
+      if (productsAmount) {
+        let itemsInStorage: string | null = localStorage.getItem('items');
+        let items: { name: string; price: string; quantity?: number }[] = itemsInStorage ? JSON.parse(itemsInStorage) : [];
+        const totalQuantity: number = items.reduce((total: number, item: { quantity?: number }) => total + (item.quantity || 0), 0);
+        productsAmount.textContent = totalQuantity.toString();
+      }
+    };
+
+    document.addEventListener("updateProductsAmount", handleUpdateProductsAmount);
+
+    // Обновляем количество товаров в шапке при загрузке страницы "AboutUsPage"
+    handleUpdateProductsAmount();
+
+    return content;
   }
 }
