50b5a2b0546ceb44b58d404daf9c117eac2fbd28
Aleksandr Gubich <a.gubich@bk.ru>
2023-12-25 18:36:10 +0300

    Готовая логика

--- src/pages/BucketPage.ts
+++ src/pages/BucketPage.ts
@@ -1,66 +1,98 @@
 import { AbstractPage } from "../router";
-import template from './BucketPage.html';
+import template from "./BucketPage.html";
 
-const templEl: HTMLTemplateElement = document.createElement('template');
-templEl.innerHTML = template;
+const templEl: HTMLTemplateElement = document.createElement("template"); // Создание элемента шаблона
+templEl.innerHTML = template; // Заполнение элемента шаблона HTML-кодом страницы корзины
 
-export class BucketPage extends AbstractPage {
-  render(): HTMLElement | DocumentFragment {
-    const renderItems = (): void => {
-      const goodsContainer: Element | null = templEl.content.querySelector('.goods');
-      if (goodsContainer) {
-        goodsContainer.innerHTML = '';
+export class BucketPage extends AbstractPage { // Объявление класса BucketPage, который расширяет AbstractPage
+  render(): HTMLElement | DocumentFragment { // Метод отображения содержимого корзины
+    const content: DocumentFragment = templEl.content.cloneNode(
+      true
+    ) as DocumentFragment; // Клонирование содержимого шаблона для отображения страницы
 
-        let itemsInStorage: string | null = localStorage.getItem('items');
-        let items: { name: string, price: string, quantity?: number }[] = itemsInStorage ? JSON.parse(itemsInStorage) : [];
+    const goodsContainer: Element | null = content.querySelector(".goods"); // Поиск контейнера товаров в корзине
 
-        let totalPrice: number = 0;
-        let totalQuantity: number = 0;
+    const renderItems = (): void => { // Функция для отображения товаров в корзине
+      if (goodsContainer) { // Проверка наличия контейнера товаров
+        goodsContainer.innerHTML = ""; // Очистка контейнера от предыдущих элементов
 
-        items.forEach((item: { name: string, price: string, quantity?: number }, index: number) => {
-          const bucketItem: HTMLDivElement = document.createElement('div');
-          bucketItem.classList.add('bucket-item');
-          bucketItem.innerHTML = `
-            <p>${item.name}</p>
-            <p>${item.price}</p>
-            <p>Количество: ${item.quantity}</p>
-            <button class="remove-btn" data-index="${index}">Удалить из корзины</button>
-          `;
-          goodsContainer.appendChild(bucketItem);
+        let itemsInStorage: string | null = localStorage.getItem("items"); // Получение данных о товарах из localStorage
+        let items: { name: string; price: string; quantity?: number }[] =
+          itemsInStorage ? JSON.parse(itemsInStorage) : []; // Преобразование данных в массив товаров
 
-          totalPrice += parseFloat(item.price.replace('$', '')) * (item.quantity || 1);
-          totalQuantity += item.quantity || 0;
-        });
+        let totalPrice: number = 0; // Инициализация общей цены товаров
+        let totalQuantity: number = 0; // Инициализация общего количества товаров
 
-        const productsAmount: Element | null = document.querySelector('.products_amount');
-        if (productsAmount) {
-          productsAmount.textContent = totalQuantity.toString();
+        items.forEach( // Перебор товаров
+          (
+            item: { name: string; price: string; quantity?: number }, // Информация о каждом товаре
+            index: number
+          ) => {
+            const bucketItem: HTMLDivElement = document.createElement("div"); // Создание элемента для товара
+            bucketItem.classList.add("bucket-item"); // Добавление класса для стилизации
+            bucketItem.innerHTML = `
+              <p>${item.name}</p>
+              <p>${item.price}</p>
+              <p>Количество: ${item.quantity}</p>
+              <button class="remove-btn" data-index="${index}">Удалить из корзины</button>
+            `; // Вставка информации о товаре и кнопки удаления в элемент товара
+            goodsContainer.appendChild(bucketItem); // Добавление товара в контейнер корзины
+
+            totalPrice +=
+              parseFloat(item.price.replace("$", "")) * (item.quantity || 1); // Вычисление общей цены товаров
+            totalQuantity += item.quantity || 0; // Вычисление общего количества товаров
+          }
+        );
+
+        const productsAmount: Element | null =
+          document.querySelector(".products_amount"); // Поиск элемента для отображения общего количества товаров
+        if (productsAmount) { // Если элемент найден
+          productsAmount.textContent = totalQuantity.toString(); // Отображение общего количества товаров
         }
 
-        const productsSum: Element | null = templEl.content.querySelector('.products_sum');
-        const priceSum: Element | null = templEl.content.querySelector('.price_sum');
-
+        // Отображение общего количества и суммы товаров в корзине
+        const productsSum: Element | null = content.querySelector(".products_sum");
+        const priceSum: Element | null = content.querySelector(".price_sum");
         if (productsSum && priceSum) {
           productsSum.textContent = totalQuantity.toString();
           priceSum.textContent = `$${totalPrice.toFixed(2)}`;
         }
-
-        goodsContainer.addEventListener('click', (event: Event) => {
-          const target = event.target as HTMLElement;
-          if (target.classList.contains('remove-btn')) {
-            const indexToRemove: number = parseInt(target.getAttribute('data-index') || '');
-            if (!isNaN(indexToRemove)) {
-              items.splice(indexToRemove, 1);
-              localStorage.setItem('items', JSON.stringify(items));
-              renderItems();
-            }
-          }
-        });
       }
     };
 
-    renderItems();
+    const handleRemoveButtonClick = (indexToRemove: number) => { // Функция обработки удаления товара
+      if (!isNaN(indexToRemove)) { // Проверка корректности индекса товара
+        let itemsInStorage: string | null = localStorage.getItem("items"); // Получение данных о товарах из localStorage
+        let items: { name: string; price: string; quantity?: number }[] =
+          itemsInStorage ? JSON.parse(itemsInStorage) : []; // Преобразование данных в массив товаров
 
-    return templEl.content.cloneNode(true) as DocumentFragment;
+        const itemToRemove = items[indexToRemove]; // Получение информации о товаре для удаления
+
+        // Проверка количества товара: уменьшение на 1, если больше одного; иначе - удаление
+        if (itemToRemove.quantity && itemToRemove.quantity > 1) {
+          itemToRemove.quantity -= 1;
+        } else {
+          items.splice(indexToRemove, 1);
+        }
+
+        localStorage.setItem("items", JSON.stringify(items)); // Обновление данных о товарах в localStorage
+        renderItems(); // Перерендеринг после удаления элемента
+      }
+    };
+
+    if (goodsContainer) { // Если контейнер товаров найден
+      goodsContainer.addEventListener("click", (event: Event) => { // Обработчик клика по контейнеру
+        const target = event.target as HTMLElement;
+        if (target.classList.contains("remove-btn")) { // Если клик был по кнопке удаления
+          const indexToRemove: number = parseInt(
+            target.getAttribute("data-index") || ""
+          ); // Получение индекса товара для удаления
+          handleRemoveButtonClick(indexToRemove); // Вызов функции удаления товара
+        }
+      });
+    }
+
+    renderItems(); // Отображение товаров при загрузке страницы
+    return content; // Возврат содержимого страницы корзины
   }
 }
--- src/pages/MainPage.ts
+++ src/pages/MainPage.ts
@@ -1,40 +1,40 @@
 import { AbstractPage } from "../router";
 import template from './MainPage.html';
 
-const templEl: HTMLTemplateElement = document.createElement('template');
-templEl.innerHTML = template;
+const templEl: HTMLTemplateElement = document.createElement('template'); // Создание элемента шаблона
+templEl.innerHTML = template; // Заполнение элемента шаблона HTML-кодом главной страницы
 
-export class MainPage extends AbstractPage {
-  render(): HTMLElement | DocumentFragment {
-    const content: DocumentFragment = templEl.content.cloneNode(true) as DocumentFragment;
+export class MainPage extends AbstractPage { // Объявление класса MainPage, который расширяет AbstractPage
+  render(): HTMLElement | DocumentFragment { // Метод отображения содержимого главной страницы
+    const content: DocumentFragment = templEl.content.cloneNode(true) as DocumentFragment; // Клонирование содержимого шаблона для отображения страницы
 
-    const toBucketButtons: NodeListOf<HTMLButtonElement> = content.querySelectorAll(".toBucket button");
+    const toBucketButtons: NodeListOf<HTMLButtonElement> = content.querySelectorAll(".toBucket button"); // Поиск кнопок перехода в корзину
 
-    toBucketButtons.forEach((button: HTMLButtonElement) => {
-      button.addEventListener('click', () => {
-        const product: Element | null = button.closest('.product');
-        if (product) {
-          const productName: string = product.querySelector('.name')?.textContent || '';
-          const productPrice: string = product.querySelector('.price')?.textContent || '';
+    toBucketButtons.forEach((button: HTMLButtonElement) => { // Для каждой кнопки установить обработчик клика
+      button.addEventListener('click', () => { // Обработчик клика по кнопке
+        const product: Element | null = button.closest('.product'); // Находим ближайший элемент товара, связанный с кнопкой
+        if (product) { // Если товар найден
+          const productName: string = product.querySelector('.name')?.textContent || ''; // Получение имени товара
+          const productPrice: string = product.querySelector('.price')?.textContent || ''; // Получение цены товара
 
-          const item: { name: string, price: string, quantity: number } = { name: productName, price: productPrice, quantity: 1 };
+          const item: { name: string, price: string, quantity: number } = { name: productName, price: productPrice, quantity: 1 }; // Создание объекта товара
 
-          let itemsInStorage: string | null = localStorage.getItem('items');
-          let items = itemsInStorage ? JSON.parse(itemsInStorage) : [];
+          let itemsInStorage: string | null = localStorage.getItem('items'); // Получение данных о товарах из localStorage
+          let items = itemsInStorage ? JSON.parse(itemsInStorage) : []; // Преобразование данных в массив товаров
 
-          const existingItemIndex = items.findIndex((item: any) => item.name === productName);
+          const existingItemIndex = items.findIndex((item: any) => item.name === productName); // Поиск индекса существующего товара
 
-          if (existingItemIndex !== -1) {
-            if (items[existingItemIndex].quantity !== undefined) {
-              items[existingItemIndex].quantity += 1;
+          if (existingItemIndex !== -1) { // Если товар уже существует в корзине
+            if (items[existingItemIndex].quantity !== undefined) { // Если у товара указано количество
+              items[existingItemIndex].quantity += 1; // Увеличиваем количество товара
             } else {
-              items[existingItemIndex].quantity = 1;
+              items[existingItemIndex].quantity = 1; // Устанавливаем количество товара
             }
           } else {
-            items.push(item);
+            items.push(item); // Добавляем товар в корзину
           }
 
-          localStorage.setItem('items', JSON.stringify(items));
+          localStorage.setItem('items', JSON.stringify(items)); // Сохраняем обновленные данные о товарах в localStorage
           this.updateProductsAmount(); // Обновляем количество товаров в шапке
         }
       });
@@ -42,16 +42,17 @@ export class MainPage extends AbstractPage {
 
     this.updateProductsAmount(); // Обновляем количество товаров в шапке при загрузке страницы
 
-    return content;
+    return content; // Возврат содержимого страницы
   }
 
-  private updateProductsAmount(): void {
-    const productsAmount: Element | null = document.querySelector('.products_amount');
-    if (productsAmount) {
-      let itemsInStorage: string | null = localStorage.getItem('items');
-      let items: { name: string, price: string, quantity?: number }[] = itemsInStorage ? JSON.parse(itemsInStorage) : [];
-      const totalQuantity: number = items.reduce((total: number, item: { quantity?: number }) => total + (item.quantity || 0), 0);
-      productsAmount.textContent = totalQuantity.toString();
+  private updateProductsAmount(): void { // Метод обновления количества товаров в шапке
+    const productsAmount: Element | null = document.querySelector('.products_amount'); // Поиск элемента для отображения количества товаров
+    if (productsAmount) { // Если элемент найден
+      let itemsInStorage: string | null = localStorage.getItem('items'); // Получение данных о товарах из localStorage
+      let items: { name: string, price: string, quantity?: number }[] = itemsInStorage ? JSON.parse(itemsInStorage) : []; // Преобразование данных в массив товаров
+      const totalQuantity: number = items.reduce((total: number, item: { quantity?: number }) => total + (item.quantity || 0), 0); // Вычисление общего количества товаров
+
+      productsAmount.textContent = totalQuantity.toString(); // Отображение общего количества товаров
     }
   }
 }
